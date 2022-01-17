bc08b6f1c231b8afae7245e45023df50c035f5ae
정재훈 <jhc060109@naver.com>
2022-01-18 05:37:23 +0900

    tutorial finnished, but will continue adding stuffs

--- src/App.js
+++ src/App.js
@@ -1,64 +1,106 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState, useEffect } from "react";
+import Footer from "./components/Footer"
+import About from "./components/About";
+import { useState, useEffect, useRef } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const JSON_URL = useRef("http://localhost:8000");
 
   useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchData();
+      const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     }
 
     getTasks();
   }, []);
 
-  const fetchData = async () => {
-    const response = await fetch("http://localhost:8000/tasks");
+  const fetchTasks = async () => {
+    const response = await fetch(`${JSON_URL.current}/tasks`);
+    const data = await response.json();
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const response = await fetch(`${JSON_URL.current}/tasks/${id}`);
     const data = await response.json();
     return data;
   }
 
   const [isAddTask, setIsAddTask] = useState(false);
 
-  const addTask = (task) => {
-    const latestId = tasks[tasks.length - 1].id;
-    const newTask = {...task, id: latestId + 1};
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const response = await fetch(`${JSON_URL.current}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(task)
+    });
+
+    const data = await response.json();
+    setTasks([...tasks, data]);
   }
 
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:8000/tasks/${id}`, {method: "DELETE"});
+    await fetch(`${JSON_URL.current}/tasks/${id}`, { method: "DELETE" });
     setTasks(tasks.filter(task => task.id !== id));
   }
 
-  const toggleReminder = (id) => {
-   setTasks(tasks.map(task => 
-      task.id === id? {...task, reminder: !task.reminder} : task
-    ));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const toggledTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+
+    const response = await fetch(`${JSON_URL.current}/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(toggledTask),
+    });
+
+    const resultTask = await response.json();
+
+    setTasks(tasks.map((task) => task.id === resultTask.id ? resultTask : task));
   }
 
   return (
-    <div className="container">
-      <Header
-        title="Task Tracker"
-        onAdd={() => setIsAddTask(!isAddTask) }
-        isAddTask={isAddTask}
-      />
-      {isAddTask && <AddTask onAddTask={addTask} />}
-      {
-        tasks.length > 0?
-          <Tasks
-            tasks={tasks}
-            onDelete={deleteTask}
-            onToggleReminder={toggleReminder}
-          />
-        :
-          "No tasks to show"
-      }
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header
+          title="Task Tracker"
+          onAdd={() => setIsAddTask(!isAddTask)}
+          isAddTask={isAddTask}
+        />
+        <Routes>
+          <Route path="/" element={
+            <>
+              {isAddTask && <AddTask onAddTask={addTask} />}
+              {
+                tasks.length > 0 ?
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggleReminder={toggleReminder}
+                  />
+                  :
+                  "No tasks to show"
+              }
+            </>
+          } />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<><h1>404 Page Not Found</h1></>} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
