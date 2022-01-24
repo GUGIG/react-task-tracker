ee9c8b2bbf4ab427cbd10297951d681865c2db04
정재훈 <jhc060109@naver.com>
2022-01-25 03:32:43 +0900

    feat: footer 컴포넌트 pathname에 따른 side effect function 추가 및 적용
    pathname이 바뀔 때마다 pathname을 비교하여 각 경우에 맞는 링크를 뜨도록 한다.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 const Footer = () => {
+    const { pathname } = useLocation();
+    const [link, setLink] = useState("/about");
+    const [linkText, setLinkText] = useState("About")
+
+    useEffect(() => {
+        if (pathname === "/") {
+            setLink("/about");
+            setLinkText("About")
+        } else if (pathname === "/about") {
+            setLink("/")
+            setLinkText("Home");
+        } else {
+            console.log("pathname >> ", pathname)
+        }
+    }, [pathname]);
+
     return (
         <footer>
             <p>Copyright &copy; 2022</p>
-            <Link to="/about">About</Link>
+            <Link to={link}>{linkText}</Link>
         </footer>
     )
 }
