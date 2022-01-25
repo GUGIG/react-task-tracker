a56ab56519bdbf6f136356cfca162105f286ea80
정재훈 <jhc060109@naver.com>
2022-01-26 07:19:24 +0900

    foramtting (prettier 2 spaces)
    formatting 및 콘솔문 제거

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,29 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
-import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
-    const { pathname } = useLocation();
-    const [link, setLink] = useState("/about");
-    const [linkText, setLinkText] = useState("About")
+  const { pathname } = useLocation();
+  const [link, setLink] = useState("/about");
+  const [linkText, setLinkText] = useState("About");
 
-    useEffect(() => {
-        if (pathname === "/") {
-            setLink("/about");
-            setLinkText("About")
-        } else if (pathname === "/about") {
-            setLink("/")
-            setLinkText("Home");
-        } else {
-            console.log("pathname >> ", pathname)
-        }
-    }, [pathname]);
+  useEffect(() => {
+    if (pathname === "/") {
+      setLink("/about");
+      setLinkText("About");
+    } else if (pathname === "/about") {
+      setLink("/");
+      setLinkText("Home");
+    } else {
+    }
+  }, [pathname]);
 
-    return (
-        <footer>
-            <p>Copyright &copy; 2022</p>
-            <Link to={link}>{linkText}</Link>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <p>Copyright &copy; 2022</p>
+      <Link to={link}>{linkText}</Link>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
