b2d98f9faddc1161ecf390569db8c9f443bcef81
정재훈 <jhc060109@naver.com>
2022-01-25 17:12:53 +0900

    formatting via prettier

--- src/App.js
+++ src/App.js
@@ -1,83 +1,91 @@
 import Header from "./components/Header";
 import Home from "./Pages/Home";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import About from "./Pages/About";
 import Version from "./Pages/Version";
 import Author from "./Pages/Author";
 import { useState, useEffect, useRef } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { UserContext } from "./Contexts/Contexts";
+import Test from "./components/Test";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const JSON_URL = useRef("http://localhost:8000");
-  
+
   const fetchTasks = async (username) => {
     const response = await fetch(`${JSON_URL.current}/${username}`);
     const tasks = await response.json();
     return tasks;
-  }
+  };
   const fetchTask = async (username, taskId) => {
     const response = await fetch(`${JSON_URL.current}/${username}/${taskId}`);
     const task = await response.json();
     return task;
-  }
+  };
   const addTask = async (username, task) => {
     const response = await fetch(`${JSON_URL.current}/${username}`, {
       method: "POST",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(task)
+      body: JSON.stringify(task),
     });
 
     const data = await response.json();
     setTasks([...tasks, data]);
-  }
+  };
   const deleteTask = async (username, taskId) => {
-    await fetch(`${JSON_URL.current}/${username}/${taskId}`, { method: "DELETE" });
-    setTasks(tasks.filter(task => task.id !== taskId));
-  }
+    await fetch(`${JSON_URL.current}/${username}/${taskId}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
   const toggleReminder = async (username, taskId) => {
     const taskToToggle = await fetchTask(username, taskId);
     const toggledTask = {
       ...taskToToggle,
-      reminder: !taskToToggle.reminder
-    }   
+      reminder: !taskToToggle.reminder,
+    };
 
     const response = await fetch(`${JSON_URL.current}/${username}/${taskId}`, {
       method: "PUT",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
       body: JSON.stringify(toggledTask),
     });
 
     const resultTask = await response.json();
 
-    setTasks(tasks.map((task) => task.id === resultTask.id ? resultTask : task));
-  }
+    setTasks(
+      tasks.map((task) => (task.id === resultTask.id ? resultTask : task))
+    );
+  };
 
   const [isAddTask, setIsAddTask] = useState(false);
-  
+
   const [userGlobalState, setUserGlobalState] = useState({
     name: "jaehun",
   });
   const toggleUserName = () => {
-    if(userGlobalState.name === "jaehun") {
-      setUserGlobalState({...userGlobalState, name: "cheolsu"});
+    if (userGlobalState.name === "jaehun") {
+      setUserGlobalState({ ...userGlobalState, name: "cheolsu" });
     } else {
-      setUserGlobalState({...userGlobalState, name: "jaehun" });
+      setUserGlobalState({ ...userGlobalState, name: "jaehun" });
     }
-  }
-  const userContextValue = { user: userGlobalState, toggleUser: toggleUserName};
+  };
+  const userContextValue = {
+    user: userGlobalState,
+    toggleUser: toggleUserName,
+  };
 
   useEffect(() => {
     (async () => {
       const t = await fetchTasks(userGlobalState.name);
       setTasks(t);
-    })()
+    })();
   }, [userGlobalState]);
 
   const homeProps = {
