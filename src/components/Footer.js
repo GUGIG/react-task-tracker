74397f0044901c569073926cefc1598348d29fc2
정재훈 <jhc060109@naver.com>
2022-01-26 23:47:54 +0900

    refactor: state & side effect => memo로 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,23 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+const pathnameData = {
+  "/": {
+    link: "/about",
+    linkText: "About",
+  },
+  "/about": {
+    link: "",
+    linkText: "Home",
+  },
+};
+
 const Footer = () => {
   const { pathname } = useLocation();
-  const [link, setLink] = useState("/about");
-  const [linkText, setLinkText] = useState("About");
 
-  useEffect(() => {
-    if (pathname === "/") {
-      setLink("/about");
-      setLinkText("About");
-    } else if (pathname === "/about") {
-      setLink("/");
-      setLinkText("Home");
-    } else {
-    }
-  }, [pathname]);
+  const link = useMemo(() => pathnameData[pathname].link, [pathname]);
+  const linkText = useMemo(() => pathnameData[pathname].linkText, [pathname]);
 
   return (
     <footer>
